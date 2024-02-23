61d4e766ab0fcaa3b0f34842f352d24be97194bd
BENDOVERCODE <151055030+BENDOVERCODE@users.noreply.github.com>
2024-02-23 14:06:02 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,2 +1,37 @@
-org 0x7C00
-bits 16
+KERNEL_STACK_SIZE equ 4096                  
+
+    section .bss
+    align 4                                 
+    kernel_stack:                           
+        resb KERNEL_STACK_SIZE  
+
+mov esp, kernel_stack + KERNEL_STACK_SIZE 
+
+
+global loader                   
+    			
+    MAGIC_NUMBER equ 0x1BADB002     
+    FLAGS        equ 0x0            
+    CHECKSUM     equ -MAGIC_NUMBER  
+                                    
+    KERNEL_STACK_SIZE equ 4096      
+    
+    
+    section .bss
+    align 4                         
+    kernel_stack:                   
+        resb KERNEL_STACK_SIZE      
+    
+        
+    section .text:                  
+    align 4                         
+        dd MAGIC_NUMBER             
+        dd FLAGS                    
+        dd CHECKSUM                 
+
+
+    loader:                                         
+        mov esp, kernel_stack + KERNEL_STACK_SIZE   
+        
+    .loop:
+        jmp .loop       
